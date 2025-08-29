b842def4be555ea7cd752fc189baba6f25ff6c20
Manuel-Guayasamin <manuelalejandro013@gmail.com>
2025-08-29 13:26:53 -0500

    actualizacion

--- lib/ui/screens/profile_page.dart
+++ lib/ui/screens/profile_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'app_colors.dart';
+import '../../core/theme/app_colors.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
